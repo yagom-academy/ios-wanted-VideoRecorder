dd4700f867368ec1c77560c06fe14369319ad4e3
Subin <skyqnaqna@naver.com>
2022-10-11 04:07:38 +0900

    feat: 첫 번째 화면 생성

---
+++ VideoRecorder/VideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  VideoModel.swift
+//  VideoRecorder
+//
+//  Created by Subin Kim on 2022/10/11.
+//
+
+import Foundation
+
+struct VideoModel {
+    let fileName: String
+    let playTime: String
+    let date: String
+}
