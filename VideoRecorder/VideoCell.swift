ac471ac54bfcba82df5f81bb38c7b0f37c705e65
권준상 <wnstkdsun97@naver.com>
2022-10-11 19:45:53 +0900

    feat : CollectionView 레이아웃 구현

---
+++ VideoRecorder/VideoCell.swift
@@ -0,0 +1,118 @@
+//
+//  VideoCell.swift
+//  VideoRecorder
+//
+//  Created by 권준상 on 2022/10/11.
+//
+
+import UIKit
+
+class VideoCell: UICollectionViewCell {
+    
+    let thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.image = UIImage(named: "sample")
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 6
+        return imageView
+    }()
+    
+    let runningTimeView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.layer.opacity = 0.6
+        view.layer.zPosition = 1
+        return view
+    }()
+    
+    let runningTimeLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 11)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.layer.zPosition = 2
+        return label
+    }()
+    
+    let labelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .leading
+        stackView.spacing = 7
+        return stackView
+    }()
+    
+    let videoNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 15, weight: .bold)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .darkGray
+        label.font = .systemFont(ofSize: 13)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let nextImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "chevron.right")
+        imageView.tintColor = .gray
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        contentView.addSubviews(thumbnailImageView, labelStackView, nextImageView)
+        thumbnailImageView.addSubview(runningTimeView)
+        runningTimeView.addSubview(runningTimeLabel)
+        labelStackView.addArrangedSubview(videoNameLabel)
+        labelStackView.addArrangedSubview(dateLabel)
+        [thumbnailImageView, labelStackView, nextImageView, runningTimeView, runningTimeLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        NSLayoutConstraint.activate([
+            thumbnailImageView.widthAnchor.constraint(equalToConstant: 80),
+            thumbnailImageView.heightAnchor.constraint(equalTo: thumbnailImageView.widthAnchor, multiplier: 0.66),
+            thumbnailImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            thumbnailImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            runningTimeView.leadingAnchor.constraint(equalTo: thumbnailImageView.leadingAnchor, constant: 5),
+            runningTimeView.bottomAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor, constant: -5),
+            runningTimeLabel.leadingAnchor.constraint(equalTo: runningTimeView.leadingAnchor, constant: 5),
+            runningTimeLabel.topAnchor.constraint(equalTo: runningTimeView.topAnchor, constant: 3),
+            runningTimeLabel.trailingAnchor.constraint(equalTo: runningTimeView.trailingAnchor, constant: -5),
+            runningTimeLabel.bottomAnchor.constraint(equalTo: runningTimeView.bottomAnchor, constant: -3),
+            labelStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+            labelStackView.leadingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: 20),
+            labelStackView.trailingAnchor.constraint(equalTo: nextImageView.leadingAnchor, constant: -10),
+            labelStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+            nextImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            nextImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -30),
+            nextImageView.widthAnchor.constraint(equalToConstant: 15),
+            nextImageView.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        views.forEach { addSubview($0) }
+    }
+}
--- VideoRecorder/ViewController.swift
+++ VideoRecorder/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createBasicListLayout())
+        collectionView.register(VideoCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, String>!
+    
+    var arr = ["Zedd", "Alan Walker", "David Guetta", "Avicii", "Marshmello", "Steve Aoki", "R3HAB", "Armin van Buuren", "Skrillex", "Illenium", "TheChainsmokerslkdjflksdjflsldkjflskd", "Don Diablo", "Afrojack", "Tiesto", "KSHMR", "DJ Snake", "Kygo", "Galantis", "Major Lazer", "Vicetone"]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        setCell()
+        performQuery()
+    }
+    
+    func createBasicListLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                             heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+      
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .estimated(90))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                         subitems: [item])
+        let spacing = CGFloat(10)
+        group.interItemSpacing = .fixed(spacing)
+      
+        let section = NSCollectionLayoutSection(group: group)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
+    func setCell() {
+        self.dataSource = UICollectionViewDiffableDataSource<Section, String>(collectionView: self.collectionView) { (collectionView, indexPath, dj: String) -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? VideoCell else { return UICollectionViewCell() }
+            cell.videoNameLabel.text = dj
+            cell.dateLabel.text = "2022-10-11"
+            cell.runningTimeLabel.text = "1:03"
+            return cell
+        }
+    }
+    
+    func performQuery() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(arr)
+        self.dataSource.apply(snapshot, animatingDifferences: true)
     }
 
 
 }
 
+enum Section: CaseIterable {
+    case main
+}
