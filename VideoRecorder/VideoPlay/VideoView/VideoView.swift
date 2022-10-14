81dbcf61d355a8186b9e2e8de9b9fff7120c1d9a
julia0926 <kj980926@naver.com>
2022-10-14 18:47:22 +0900

    feature : video slider / time 설정

--- VideoRecorder/VideoPlay/VideoView/VideoView.swift
+++ VideoRecorder/VideoPlay/VideoView/VideoView.swift
@@ -13,6 +13,7 @@ final class VideoView: UIView {
     
     @IBOutlet private weak var backgroundView: UIView!
     //playView
+    @IBOutlet private weak var playView: UIView!
     @IBOutlet private weak var videoSlider: UISlider!
     @IBOutlet private weak var nowTimeLabel: UILabel!
     @IBOutlet private weak var totalTimeLabel: UILabel!
@@ -20,7 +21,14 @@ final class VideoView: UIView {
     
     private var player = AVPlayer()
     private var playerLayer: AVPlayerLayer?
+    private var playItem: AVPlayerItem?
     private let url: String = "https://bitmovin-a.akamaihd.net/content/art-of-motion_drm/m3u8s/11331.m3u8"
+    private var videoIsPlay: Bool = false
+    private var playerItemContext = 0
+    private let requiredAssetKeys = [
+        "playable",
+        "hasProtectedContent"
+    ]
     
     override func layoutSubviews() {
       super.layoutSubviews()
@@ -39,31 +47,97 @@ final class VideoView: UIView {
         setVideoView()
     }
     
-    func setVideoView() {
+    @IBAction func tappedPlayButton(_ sender: UIButton) {
+        self.videoIsPlay.toggle()
+        if videoIsPlay == true {
+            self.player.play()
+            self.playButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        } else {
+            self.player.pause()
+            self.playButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        }
+
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?,
+                               of object: Any?,
+                               change: [NSKeyValueChangeKey : Any]?,
+                               context: UnsafeMutableRawPointer?) {
+
+        guard context == &playerItemContext else {
+            super.observeValue(forKeyPath: keyPath,
+                               of: object,
+                               change: change,
+                               context: context)
+            return
+        }
+
+        if keyPath == #keyPath(AVPlayerItem.status) {
+            let status: AVPlayerItem.Status
+            if let statusNumber = change?[.newKey] as? NSNumber {
+                status = AVPlayerItem.Status(rawValue: statusNumber.intValue)!
+            } else {
+                status = .unknown
+            }
+            guard let item = playItem else {
+                print("no playItem!!!")
+                return
+            }
+            // Switch over status value
+            switch status {
+            case .readyToPlay:
+                print("ready!!!")
+                self.playView.isHidden = false
+                self.videoSlider.minimumValue = 0
+                self.videoSlider.maximumValue = Float(CMTimeGetSeconds(item.duration))
+            case .failed:
+                debugPrint("Player item failed. See error.")
+            default:
+                debugPrint("Player item is not yet ready.")
+            }
+        }
+    }
+    
+    private func setVideoView() {
         guard let url = URL(string: self.url) else { return }
-        let item = AVPlayerItem(url: url)
-        self.player.replaceCurrentItem(with: item)
+        let asset = AVAsset(url: url)
+        playItem = AVPlayerItem(asset: asset,
+                                automaticallyLoadedAssetKeys: requiredAssetKeys)
+        playItem?.addObserver(self,
+                                   forKeyPath: #keyPath(AVPlayerItem.status),
+                                   options: [.old, .new],
+                                   context: &playerItemContext)
+        self.player.replaceCurrentItem(with: playItem)
         let playerLayer = AVPlayerLayer(player: self.player)
         playerLayer.frame = self.backgroundView.bounds
-        playerLayer.videoGravity = .resizeAspectFill
+        playerLayer.videoGravity = .resize
         self.playerLayer = playerLayer
         self.backgroundView.layer.addSublayer(playerLayer)
-        self.player.play()
-       
-        if self.player.status == .readyToPlay {
-            self.videoSlider.minimumValue = 0
-            self.videoSlider.maximumValue = Float(CMTimeGetSeconds(item.duration))
-        }
- 
-        
+        self.settingIntevalPlayTime()
+        self.videoSlider.addTarget(self, action: #selector(didChangedSliderValue), for: .valueChanged)
+    }
+
+    private func settingIntevalPlayTime() {
         let interval = CMTimeMakeWithSeconds(1, preferredTimescale: Int32(NSEC_PER_SEC))
         self.player.addPeriodicTimeObserver(forInterval: interval, queue: .main, using: { [weak self] elapsedSeconds in
             let elapsedTimeSecondsFloat = CMTimeGetSeconds(elapsedSeconds)
             let totalTimeSecondsFloat = CMTimeGetSeconds(self?.player.currentItem?.duration ?? CMTimeMake(value: 1, timescale: 1))
-            self?.nowTimeLabel.text = String(elapsedTimeSecondsFloat)
-            self?.totalTimeLabel.text = String(totalTimeSecondsFloat)
-            
-            print(elapsedTimeSecondsFloat, totalTimeSecondsFloat)
+            if !elapsedTimeSecondsFloat.isNaN && !totalTimeSecondsFloat.isNaN {
+                self?.nowTimeLabel.text = self?.changeFloatToTime(elapsedTimeSecondsFloat)
+                self?.totalTimeLabel.text = self?.changeFloatToTime(totalTimeSecondsFloat)
+            }
+        })
+    }
+    
+    private func changeFloatToTime(_ value: Float64) -> String {
+        let minute = Int(value) / 60
+        let second = Int(value) % 60
+        return NSString(format: "%02d:%02d", minute, second) as String
+    }
+    
+    @objc func didChangedSliderValue() {
+        self.player.seek(to: CMTime(seconds: Double(self.videoSlider.value), preferredTimescale: Int32(NSEC_PER_SEC)), completionHandler: { _ in
+          print("\(self.videoSlider.value)")
         })
     }
     
