d0564096565cb38a8b235dd433361b38e6d4433e
minsu3007 <minsu3007@gmail.com>
2023-06-05 22:06:54 +0900

    feat: StopCircle View

---
+++ ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/Presentation/CameraScene/View/StopCircle.swift
@@ -0,0 +1,29 @@
+//
+//  StopCircle.swift
+//  ios-wanted-VideoRecorder
+//
+//  Created by 강민수 on 2023/06/05.
+//
+
+import SwiftUI
+
+struct StopCircle: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 3)
+                .frame(width: 50, height: 50)
+                .padding()
+            
+            Rectangle()
+                .foregroundColor(.red)
+                .frame(width: 30, height: 30)
+        }
+    }
+}
+
+struct StopCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        StopCircle()
+    }
+}
