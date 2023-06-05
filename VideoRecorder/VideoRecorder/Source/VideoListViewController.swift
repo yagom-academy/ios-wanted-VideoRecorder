167621fde2c34977a3f97030593fd3478b7c03cd
kkuldak1@naver.com <kkuldak1@naver.com>
2023-06-05 15:50:38 +0900

    chore: 프로젝트 생성, 폴더 구조화

---
+++ VideoRecorder/VideoRecorder/Source/VideoListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  VideoListViewController.swift
+//  VideoRecorder
+//
+//  Created by kokkilE on 2023/06/05.
+//
+
+import UIKit
+
+class VideoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
