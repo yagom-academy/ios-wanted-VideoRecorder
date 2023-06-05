80b5fdfb0b6f79299f2b41ca454eab9036636cb5
kkuldak1@naver.com <kkuldak1@naver.com>
2023-06-05 21:36:02 +0900

    feat: VideoListVC의 컬렉션뷰 및 레이아웃 구현

---
+++ VideoRecorder/VideoRecorder/Source/VideoList/VideoListViewController.swift
@@ -0,0 +1,81 @@
+//
+//  VideoListViewController.swift
+//  VideoRecorder
+//
+//  Created by kokkilE on 2023/06/05.
+//
+
+import UIKit
+
+class VideoListViewController: UIViewController {
+    private lazy var collectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createVideoListViewLayout())
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        setupCollectionView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupCollectionView() {
+        addSubviews()
+        setupCollectionViewConstraints()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(collectionView)
+    }
+    
+    private func setupCollectionViewConstraints() {
+        let safe = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safe.topAnchor, constant: 12),
+            collectionView.leadingAnchor.constraint(equalTo: safe.leadingAnchor, constant: 12),
+            collectionView.trailingAnchor.constraint(equalTo: safe.trailingAnchor, constant: 12),
+            collectionView.bottomAnchor.constraint(equalTo: safe.bottomAnchor, constant: 12)
+        ])
+    }
+    
+    private func createVideoListViewLayout() -> UICollectionViewLayout {
+        let videoItemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.25),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let videoitem = NSCollectionLayoutItem(layoutSize: videoItemSize)
+        
+        let descriptionItemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.75),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let descriptionItem = NSCollectionLayoutItem(layoutSize: descriptionItemSize)
+
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(44)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [videoitem, descriptionItem]
+        )
+        let spacing = CGFloat(10)
+        group.interItemSpacing = .fixed(spacing)
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = spacing
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+}
