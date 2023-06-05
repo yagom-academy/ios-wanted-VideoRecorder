b9cef33a9df5e15d189a916d113b613e05d9c942
minsu3007 <minsu3007@gmail.com>
2023-06-05 15:25:47 +0900

    refactor: SampleData

--- ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/SampleData.swift
+++ ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/SampleData.swift
@@ -5,11 +5,9 @@
 //  Created by 강민수 on 2023/06/05.
 //
 
-import Foundation
 import AVKit
 
 struct Sample {
-    
     var videos: [Video] = [
         Video(title: "Countdown.mp4", date: Date(), video: AVPlayer(url: Bundle.main.url(forResource: "countdown", withExtension: "mp4")!)),
         Video(title: "Forest.mp4", date: Date(), video: AVPlayer(url: Bundle.main.url(forResource: "forest", withExtension: "mp4")!)),
