4e9c91c475a187ecb0c9ff226cccf7a1d73cc3b2
hyemory <cjstksla73@gmail.com>
2023-06-05 23:22:26 +0900

    project setting (target, file tree)

---
+++ VideoRecorder/VideoRecorder/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  VideoRecorder
+//
+//  Created by Hyejeong Jeong on 2023/06/05.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
