2d2da0614bb118c70d28ef8aa803528346694f05
kimseongj <ksj8277@naver.com>
2023-06-06 21:04:07 +0900

    feat : NavigationBar 요구사항대로 구현

--- VideoRecoder/Source/View/VideoListViewController.swift
+++ VideoRecoder/Source/View/VideoListViewController.swift
@@ -11,19 +11,84 @@ class VideoListViewController: UIViewController {
     private enum Section: CaseIterable {
         case main
     }
+    
+    private var dataSource: UITableViewDiffableDataSource<Section, Video>!
+    
+    private let leftBarItemStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 10
+        
+        return stackView
+    }()
+    
+    private let titleIconView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "list.triangle")
+        imageView.tintColor = .black
+        
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.black
+        label.text = "Video List"
+        label.font = UIFont.preferredFont(forTextStyle: .title1, compatibleWith: UITraitCollection(legibilityWeight: .bold))
+        
+        return label
+    }()
+    
+    private let addVideoButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "video.fill.badge.plus"), for: .normal)
+        button.tintColor = .purple
+        button.imageView?.contentMode = .scaleAspectFit
+        button.imageEdgeInsets = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
     private let videoListTableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
         return tableView
     }()
-    private var dataSource: UITableViewDiffableDataSource<Section, Video>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+        configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        configureLeftBarItem()
+        configureRightBarItem()
+    }
+    
+    private func configureLeftBarItem() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: leftBarItemStackView)
+        
+        leftBarItemStackView.addArrangedSubview(titleIconView)
+        leftBarItemStackView.addArrangedSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleIconView.widthAnchor.constraint(equalToConstant: 30),
+            titleIconView.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+    }
+    
+    private func configureRightBarItem() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem.init(customView: addVideoButton)
     }
     
     private func configureUI() {
+        view.backgroundColor = .white
+        
         let safeArea = view.safeAreaLayoutGuide
         
         view.addSubview(videoListTableView)
@@ -39,15 +104,17 @@ class VideoListViewController: UIViewController {
     private func configureDataSource() {
         videoListTableView.register(VideoListCell.self, forCellReuseIdentifier: VideoListCell.identifier)
         
-        dataSource = UITableViewDiffableDataSource<Section, Video>(tableView: videoListTableView, cellProvider: { tableView, indexPath, video in
+        dataSource = UITableViewDiffableDataSource<Section, Video>(
+            tableView: videoListTableView,
+            cellProvider: { tableView, indexPath, video in
             
             let cell = tableView.dequeueReusableCell(withIdentifier: VideoListCell.identifier, for: indexPath) as? VideoListCell
             
             cell?.configureCell(video: video)
             
             return cell
-            
-        })
+            }
+        )
     }
     
     private func applySnapshot(videos: [Video]) {
@@ -57,5 +124,3 @@ class VideoListViewController: UIViewController {
         dataSource?.apply(snapshot, animatingDifferences: true)
     }
 }
-
-
