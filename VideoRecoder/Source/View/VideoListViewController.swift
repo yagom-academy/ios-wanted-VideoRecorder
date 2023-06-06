0fd25e80e8e3e914779513ad6315464e9e90e619
kimseongj <ksj8277@naver.com>
2023-06-06 16:13:06 +0900

    feat: DiffableDataSource를 사용하여 TableView 구현

--- VideoRecoder/Source/View/VideoListViewController.swift
+++ VideoRecoder/Source/View/VideoListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  VideoListViewController.swift
 //  VideoRecoder
 //
 //  Created by kimseongjun on 2023/06/05.
@@ -11,14 +11,18 @@ class VideoListViewController: UIViewController {
     private enum Section: CaseIterable {
         case main
     }
-    private let videoListTableView = UITableView()
+    private let videoListTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
     private var dataSource: UITableViewDiffableDataSource<Section, Video>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-
+    
     private func configureUI() {
         let safeArea = view.safeAreaLayoutGuide
         
@@ -31,5 +35,27 @@ class VideoListViewController: UIViewController {
             videoListTableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
         ])
     }
+    
+    private func configureDataSource() {
+        videoListTableView.register(VideoListCell.self, forCellReuseIdentifier: VideoListCell.identifier)
+        
+        dataSource = UITableViewDiffableDataSource<Section, Video>(tableView: videoListTableView, cellProvider: { tableView, indexPath, video in
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: VideoListCell.identifier, for: indexPath) as? VideoListCell
+            
+            cell?.configureCell(video: video)
+            
+            return cell
+            
+        })
+    }
+    
+    private func applySnapshot(videos: [Video]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Video>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(videos)
+        dataSource?.apply(snapshot, animatingDifferences: true)
+    }
 }
+
 
