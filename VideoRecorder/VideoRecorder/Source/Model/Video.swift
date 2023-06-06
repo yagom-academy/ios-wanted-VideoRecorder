3ec6a1806e9db1e22cf209e243d7afe6bc45e337
kkuldak1@naver.com <kkuldak1@naver.com>
2023-06-06 11:23:17 +0900

    feat: Video 모델이 Hashable 프로토콜 채택, ReuseIdentifying 프로토콜 구현

---
+++ VideoRecorder/VideoRecorder/Source/Model/Video.swift
@@ -0,0 +1,14 @@
+//
+//  Video.swift
+//  VideoRecorder
+//
+//  Created by kokkilE on 2023/06/06.
+//
+
+import UIKit
+
+struct Video: Hashable {
+    let image: UIImage
+    let name: String
+    let date: String
+}
