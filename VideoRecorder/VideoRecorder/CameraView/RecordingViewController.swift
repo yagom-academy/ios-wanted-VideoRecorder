6d46a35cc8d1a6eaf59c7b598e8464ce9a68e2eb
ROWAN <kimkj93@naver.com>
2023-06-06 15:33:30 +0900

    refactor: 스택뷰 내부 이미지뷰 크기 조절이 되지 않는 부분 수정 중

--- VideoRecorder/VideoRecorder/CameraView/RecordingViewController.swift
+++ VideoRecorder/VideoRecorder/CameraView/RecordingViewController.swift
@@ -11,7 +11,8 @@ import AVFoundation
 final class RecordingViewController: UIViewController {
     private let closeButton = {
         let button = UIButton()
-        button.setImage(UIImage(systemName: "xmark.circle.fill"), for: .normal)
+        var image = UIImage(systemName: "xmark.circle.fill")
+        button.setImage(image, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         
         return button
@@ -19,6 +20,7 @@ final class RecordingViewController: UIViewController {
     private let swapButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "arrow.triangle.2.circlepath.camera"), for: .normal)
+        button.setContentHuggingPriority(.required, for: .horizontal)
         
         return button
     }()
@@ -26,12 +28,44 @@ final class RecordingViewController: UIViewController {
         let label = UILabel()
         label.textColor = .white
         label.font = .preferredFont(forTextStyle: .body)
+        label.textAlignment = .center
+        label.text = "00:00"
+        label.setContentHuggingPriority(.required, for: .vertical)
+        
         return label
     }()
-    private let thumbnailView = UIImageView(frame: CGRect(x: .zero, y: .zero, width: 45, height: 45))
+    private let thumbnailView = {
+        let imageView = UIImageView()
+        imageView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    private let recordingStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .fill
+        stackView.setContentCompressionResistancePriority(.required, for: .horizontal)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 30
+        
+        return stackView
+    }()
+    private let opaqueView = {
+        let opaqueView = UIView()
+        opaqueView.backgroundColor = .separator
+        opaqueView.layer.cornerRadius = 15
+        opaqueView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return opaqueView
+    }()
+    
     private let recordButton = RecordingButton()
     
-    private lazy var previewLayer = AVCaptureVideoPreviewLayer(session: self.viewModel.captureSession())
+    private lazy var previewLayer = AVCaptureVideoPreviewLayer(session: viewModel.captureSession())
 
     private let viewModel: RecordingViewModel
     
@@ -49,28 +83,24 @@ final class RecordingViewController: UIViewController {
         super.viewDidLoad()
         configureRootView()
         setupViewHierarchy()
+        setupLayoutConstraints()
     }
     
     private func configureRootView() {
+        view.backgroundColor = .systemBackground
         view.layer.addSublayer(previewLayer)
     }
     
     private func setupViewHierarchy() {
         let recordButtonStackView = UIStackView(arrangedSubviews: [recordButton, timerLabel])
         recordButtonStackView.axis = .vertical
-        recordButtonStackView.alignment = .center
-        recordButtonStackView.distribution = .equalSpacing
+        recordButtonStackView.alignment = .fill
+        recordButtonStackView.distribution = .fill
         
-        let recordingStackView = UIStackView(arrangedSubviews: [
-            thumbnailView, recordButtonStackView, swapButton
-        ])
-        recordingStackView.axis = .horizontal
-        recordingStackView.alignment = .center
-        recordingStackView.distribution = .equalSpacing
+        recordingStackView.addArrangedSubview(thumbnailView)
+        recordingStackView.addArrangedSubview(recordButtonStackView)
+        recordingStackView.addArrangedSubview(swapButton)
         
-        let opaqueView = UIView()
-        opaqueView.alpha = 0.3
-        opaqueView.backgroundColor = .black
         opaqueView.addSubview(recordingStackView)
     
         view.addSubview(opaqueView)
@@ -79,7 +109,21 @@ final class RecordingViewController: UIViewController {
     
     private func setupLayoutConstraints() {
         NSLayoutConstraint.activate([
+            thumbnailView.heightAnchor.constraint(equalToConstant: 20),
+            thumbnailView.widthAnchor.constraint(equalTo: thumbnailView.heightAnchor, multiplier: 1),
             
+            closeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+            closeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            recordingStackView.topAnchor.constraint(equalTo: opaqueView.topAnchor, constant: 10),
+            recordingStackView.bottomAnchor.constraint(equalTo: opaqueView.bottomAnchor, constant: -10),
+            recordingStackView.leadingAnchor.constraint(equalTo: opaqueView.leadingAnchor, constant: 20),
+            recordingStackView.trailingAnchor.constraint(equalTo: opaqueView.trailingAnchor, constant: -20),
+            
+            opaqueView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            opaqueView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            opaqueView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -70),
+            opaqueView.topAnchor.constraint(equalTo: view.topAnchor, constant: 650)
         ])
     }
 }
@@ -91,7 +135,7 @@ final class RecordingButton: UIButton {
         let centerPoint = CGPoint(x: width / 2, y: height / 2)
         let outterCirclePath = UIBezierPath(
             arcCenter: centerPoint,
-            radius: width / 2 - 4,
+            radius: width / 2 - 18,
             startAngle: .zero,
             endAngle: .pi * 2,
             clockwise: true
@@ -102,7 +146,7 @@ final class RecordingButton: UIButton {
         
         let innerCirclePath = UIBezierPath(
             arcCenter: CGPoint(x: width / 2, y: height / 2),
-            radius: width / 2 - 13,
+            radius: width / 2 - 25,
             startAngle: .zero,
             endAngle: .pi * 2,
             clockwise: true
