25463b8fadc094f9f78084ee8b913a76372bbdc2
minsu3007 <minsu3007@gmail.com>
2023-06-07 13:26:59 +0900

    feat: searchVideos method by Realm

--- ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/Presentation/MainScene/ViewModel/MainViewModel.swift
+++ ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/Presentation/MainScene/ViewModel/MainViewModel.swift
@@ -9,7 +9,22 @@ import Combine
 import Foundation
 
 final class MainViewModel: ObservableObject {
-    @Published var videos: [Video] = Sample().videos
+    @Published var videos: [Video] = []
+    private let realmManager = RealmManager<VideoObject>()
+    
+    init() {
+        searchVideos()
+    }
+    
+    private func searchVideos() {
+        guard let realmObjects = realmManager.read() else { return }
+        
+        let videos = realmObjects.compactMap { object in
+            return Video.toObject(object)
+        }
+        
+        self.videos = videos
+    }
     
     func deleteVideo(_ id: UUID) {
         guard let index = videos.firstIndex(where: { video in
@@ -17,7 +32,11 @@ final class MainViewModel: ObservableObject {
         }) else {
             return
         }
-        
+        let video = videos[index]
         videos.remove(at: index)
+        
+        guard let videoObject = Video.toRealmObject(video) as? VideoObject else { return }
+        
+        realmManager.delete(videoObject)
     }
 }
