12d5005da1ab7784f969a25867ea0a6e3b3d9636
seunghyunCheon <csh7215@naver.com>
2023-06-07 15:19:38 +0900

    feat: videoListController UI구현
    videoCell UI수정

--- ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/VideoScene/VideoCell.swift
+++ ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/VideoScene/VideoCell.swift
@@ -10,23 +10,45 @@ import UIKit
 final class VideoCell: UICollectionViewListCell {
     private var imageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.image = UIImage(named: "mockImage")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
         
         return imageView
     }()
     
     private var imageLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 12)
         label.text = "3:21"
-        label.backgroundColor = .black
         label.textColor = .white
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
     }()
     
+    private var imageLabelView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        view.layer.cornerRadius = 4
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private var imageContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
     private var titleLabel: UILabel = {
         let label = UILabel()
         label.text = "Nature.mp4"
+        label.textColor = .black
         label.font = UIFont.preferredFont(forTextStyle: .title2)
         label.adjustsFontForContentSizeCategory = true
         
@@ -37,7 +59,7 @@ final class VideoCell: UICollectionViewListCell {
         let label = UILabel()
         label.text = "2022-09-22"
         label.font = UIFont.preferredFont(forTextStyle: .body)
-        label.textColor = .systemGray6
+        label.textColor = .systemGray
         label.adjustsFontForContentSizeCategory = true
         
         return label
@@ -46,14 +68,20 @@ final class VideoCell: UICollectionViewListCell {
     private let informationStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.spacing = 20
+        stackView.distribution = .fillEqually
         
         return stackView
     }()
     
     private let moreButton: UIButton = {
         let button = UIButton()
-        button.setImage(UIImage(systemName: "more"), for: .normal)
+        let largeConfig = UIImage.SymbolConfiguration(pointSize: 20, weight: .bold, scale: .default)
+        button.setImage(UIImage(systemName: "ellipsis", withConfiguration: largeConfig), for: .normal)
+        button.tintColor = .systemGray
+        button.layer.shadowOpacity = 0.2
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 2, height: 4)
+        button.layer.shadowRadius = 1
         
         return button
     }()
@@ -66,6 +94,8 @@ final class VideoCell: UICollectionViewListCell {
         return stackView
     }()
     
+    static let identifier = "cell"
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureContentLayout()
@@ -75,12 +105,44 @@ final class VideoCell: UICollectionViewListCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func provide(_ video: Video) {
+        titleLabel.text = video.name
+        dateLabel.text = video.date.description
+    }
+    
     private func configureContentLayout() {
-        imageView.addSubview(imageLabel)
+        contentView.addSubview(imageContainerView)
         
         NSLayoutConstraint.activate([
-            imageLabel.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: -10),
-            imageLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: 10)
+            imageContainerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            imageContainerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            imageContainerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20),
+            imageContainerView.widthAnchor.constraint(equalToConstant: 90),
+            imageContainerView.heightAnchor.constraint(equalToConstant: 70),
+        ])
+        
+        imageLabelView.addSubview(imageLabel)
+        
+        imageContainerView.addSubview(imageView)
+        imageContainerView.addSubview(imageLabelView)
+        
+        
+        NSLayoutConstraint.activate([
+            imageLabelView.leadingAnchor.constraint(equalTo: imageContainerView.leadingAnchor, constant: 5),
+            imageLabelView.bottomAnchor.constraint(equalTo: imageContainerView.bottomAnchor, constant: -5),
+            imageLabelView.widthAnchor.constraint(greaterThanOrEqualToConstant: 15),
+            imageLabelView.heightAnchor.constraint(greaterThanOrEqualToConstant: 5),
+            
+            imageLabel.topAnchor.constraint(equalTo: imageLabelView.topAnchor, constant: 2),
+            imageLabel.bottomAnchor.constraint(equalTo: imageLabelView.bottomAnchor, constant: -2),
+            imageLabel.leadingAnchor.constraint(equalTo: imageLabelView.leadingAnchor, constant: 2),
+            imageLabel.trailingAnchor.constraint(equalTo: imageLabelView.trailingAnchor, constant: -2),
+            
+            imageView.leadingAnchor.constraint(equalTo: imageContainerView.leadingAnchor),
+            imageView.topAnchor.constraint(equalTo: imageContainerView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: imageContainerView.bottomAnchor),
+            imageView.widthAnchor.constraint(equalTo: imageContainerView.widthAnchor),
+            imageView.heightAnchor.constraint(equalTo: imageContainerView.heightAnchor)
         ])
         
         informationStackView.addArrangedSubview(titleLabel)
@@ -89,17 +151,12 @@ final class VideoCell: UICollectionViewListCell {
         contentStackView.addArrangedSubview(informationStackView)
         contentStackView.addArrangedSubview(moreButton)
         
-        contentView.addSubview(imageView)
         contentView.addSubview(contentStackView)
         
         NSLayoutConstraint.activate([
-            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
-            imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
-            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20),
-            
-            contentStackView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: 10),
-            contentStackView.topAnchor.constraint(equalTo: imageView.topAnchor, constant: 10),
-            contentStackView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: -10),
+            contentStackView.leadingAnchor.constraint(equalTo: imageContainerView.trailingAnchor, constant: 10),
+            contentStackView.topAnchor.constraint(equalTo: imageContainerView.topAnchor),
+            contentStackView.bottomAnchor.constraint(equalTo: imageContainerView.bottomAnchor),
             contentStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10)
         ])
     }
--- ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/VideoScene/VideoListViewController.swift
+++ ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/VideoScene/VideoListViewController.swift
@@ -8,10 +8,91 @@
 import UIKit
 
 final class VideoListViewController: UIViewController {
+    private enum Section: Hashable {
+        case videos
+    }
+    
+    private typealias DataSource = UICollectionViewDiffableDataSource<Section, Video.ID>
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Video.ID>
+    
+    private lazy var videoCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewListLayout())
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.register(VideoCell.self, forCellWithReuseIdentifier: VideoCell.identifier)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
+    
+    private var dataSource: DataSource?
+    
+    private let videoList: [Video] = [
+        Video(name: "mcok", fileExtension: "mp4", date: Date(), url: "www.what.com", duration: "32:24"),
+        Video(name: "mcok", fileExtension: "mp4", date: Date(), url: "www.what.com", duration: "32:24"),
+        Video(name: "mcok", fileExtension: "mp4", date: Date(), url: "www.what.com", duration: "32:24"),
+        Video(name: "mcok", fileExtension: "mp4", date: Date(), url: "www.what.com", duration: "32:24"),
+        Video(name: "mcok", fileExtension: "mp4", date: Date(), url: "www.what.com", duration: "32:24"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureRootView()
+        configureLayout()
+        configureDataSource()
+        applySnapshot()
+    }
+    
+    private func configureRootView() {
+        view.backgroundColor = .white
+        view.addSubview(videoCollectionView)
+    }
+    
+    private func configureLayout() {
+        let safe = view.safeAreaLayoutGuide
         
- 
+        NSLayoutConstraint.activate([
+            videoCollectionView.leadingAnchor.constraint(equalTo: safe.leadingAnchor),
+            videoCollectionView.trailingAnchor.constraint(equalTo: safe.trailingAnchor),
+            videoCollectionView.topAnchor.constraint(equalTo: safe.topAnchor),
+            videoCollectionView.bottomAnchor.constraint(equalTo: safe.bottomAnchor)
+        ])
+    }
+    
+    private func configureDataSource() {
+        dataSource = DataSource(collectionView: videoCollectionView, cellProvider: cellProvider)
+    }
+    
+    private func cellProvider(_ collectionView: UICollectionView, indexPath: IndexPath, identifier: Video.ID) -> UICollectionViewCell? {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: VideoCell.identifier,
+            for: indexPath
+        ) as? VideoCell
+
+        cell?.accessories = [
+            .disclosureIndicator()
+        ]
+        
+        let video = self.videoList[indexPath.row]
+
+        cell?.provide(video)
+
+        return cell
+    }
+    
+    private func collectionViewListLayout() -> UICollectionViewLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .plain)
+        listConfiguration.backgroundColor = .clear
+
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+    
+    private func applySnapshot() {
+        let videoIDList = videoList.map { $0.id }
+        
+        var snapshot = Snapshot()
+        snapshot.appendSections([.videos])
+        snapshot.appendItems(videoIDList)
+        dataSource?.apply(snapshot)
     }
 }
 
