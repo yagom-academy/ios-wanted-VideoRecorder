12932e1f0123fe16a6dcc84db37d2f901e26b9cd
seunghyunCheon <csh7215@naver.com>
2023-06-07 09:32:30 +0900

    initial commit

---
+++ ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-wanted-VideoRecorder
+//
+//  Created by brody on 2023/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
