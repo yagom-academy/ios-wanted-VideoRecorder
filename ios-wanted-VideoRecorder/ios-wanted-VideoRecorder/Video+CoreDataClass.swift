bc4ac09d40cd6d572cc803312b952c5fc973bbd1
seunghyunCheon <csh7215@naver.com>
2023-06-07 09:58:45 +0900

    feat: 코어데이터 Video Entity 정의

---
+++ ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/Video+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Video+CoreDataClass.swift
+//  ios-wanted-VideoRecorder
+//
+//  Created by 천승현 on 2023/06/07.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Video)
+public class Video: NSManagedObject {
+
+}
