88381c4db5a63c552a85830a11567c36581ecefd
ROWAN <kimkj93@naver.com>
2023-06-08 17:07:38 +0900

    feat: VideoData 타입 정의

---
+++ VideoRecorder/VideoRecorder/Domain/VideoData.swift
@@ -0,0 +1,13 @@
+//
+//  VideoData.swift
+//  VideoRecorder
+//
+//  Created by Rowan on 2023/06/08.
+//
+
+import Foundation
+
+struct VideoData: Hashable {
+    let name: String?
+    let creationDate: Date?
+}
