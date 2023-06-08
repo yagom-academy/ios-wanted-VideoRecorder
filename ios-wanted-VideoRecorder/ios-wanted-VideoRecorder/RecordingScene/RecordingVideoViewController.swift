ec3ccfe8ca72f1b6a4cf6df6128cc55f5dc0736b
seunghyunCheon <csh7215@naver.com>
2023-06-08 18:09:02 +0900

    feat: RecordingButton 수정, timer 설정

--- ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/RecordingScene/RecordingVideoViewController.swift
+++ ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/RecordingScene/RecordingVideoViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 final class RecordingVideoViewController: UIViewController {
     private enum Constant {
-        static let ButtonWidth: CGFloat = 50
-        static let RecordingButtonWidth: CGFloat = 25
-        static let borderViewWidth: CGFloat = 70
+        static let ButtonWidth: CGFloat = 35
+        static let buttonRadius: CGFloat = 17.5
+        static let RecordingButtonWidth: CGFloat = 20
         static let recordingButtonRadius: CGFloat = 0
-        static let buttonRadius: CGFloat = 25
+        static let borderViewWidth: CGFloat = 60
     }
     
     private let borderView: UIView = {
@@ -21,65 +21,150 @@ final class RecordingVideoViewController: UIViewController {
         view.backgroundColor = .clear
         view.layer.borderWidth = 5
         view.layer.borderColor = UIColor.white.cgColor
-        view.layer.cornerRadius = 35
+        view.layer.cornerRadius = 30
         view.translatesAutoresizingMaskIntoConstraints = false
         
         return view
     }()
     
-    private let recodingButton: UIButton = {
+    private let recordingButton: UIButton = {
         let button = UIButton()
         button.setTitleColor(.black, for: .normal)
         button.backgroundColor = .red
-        button.layer.cornerRadius = Constant.RecordingButtonWidth
+        button.layer.cornerRadius = Constant.buttonRadius
         button.translatesAutoresizingMaskIntoConstraints = false
         
         return button
     }()
     
+    private let recordingPlayerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black.withAlphaComponent(0.5)
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private let timerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "00:00"
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
     private var buttonWidthConstraint: NSLayoutConstraint!
     private var buttonHeightConstraint: NSLayoutConstraint!
     
+    var timer: Timer?
+    var secondsOfTimer = 0
+    
     override func viewDidLoad() {
+        configureLayout()
+        connectTarget()
+    }
+    
+    private func configureLayout() {
         view.backgroundColor = .white
         
+        view.addSubview(recordingPlayerView)
+        
+        NSLayoutConstraint.activate([
+            recordingPlayerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            recordingPlayerView.heightAnchor.constraint(
+                equalTo: recordingPlayerView.widthAnchor,
+                multiplier: 0.35
+            ),
+            recordingPlayerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            recordingPlayerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100)
+        ])
+        
+        configureButtonLayout()
+        configureTimerLayout()
+    }
+    
+    private func configureButtonLayout() {
         view.addSubview(borderView)
         
         NSLayoutConstraint.activate([
             borderView.widthAnchor.constraint(equalToConstant: Constant.borderViewWidth),
             borderView.heightAnchor.constraint(equalToConstant: Constant.borderViewWidth),
-            borderView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            borderView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            borderView.centerXAnchor.constraint(equalTo: recordingPlayerView.centerXAnchor),
+            borderView.centerYAnchor.constraint(equalTo: recordingPlayerView.centerYAnchor, constant: -10),
         ])
         
-        view.addSubview(recodingButton)
-        buttonWidthConstraint = recodingButton.widthAnchor.constraint(equalToConstant: Constant.ButtonWidth)
-        buttonHeightConstraint = recodingButton.heightAnchor.constraint(equalToConstant: Constant.ButtonWidth)
+        view.addSubview(recordingButton)
+        buttonWidthConstraint = recordingButton.widthAnchor.constraint(equalToConstant: Constant.ButtonWidth)
+        buttonHeightConstraint = recordingButton.heightAnchor.constraint(equalToConstant: Constant.ButtonWidth)
         
         NSLayoutConstraint.activate([
             buttonWidthConstraint,
             buttonHeightConstraint,
-            recodingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            recodingButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            recordingButton.centerXAnchor.constraint(equalTo: borderView.centerXAnchor),
+            recordingButton.centerYAnchor.constraint(equalTo: borderView.centerYAnchor),
         ])
+    }
+    
+    private func configureTimerLayout() {
+        view.addSubview(timerLabel)
         
-        recodingButton.addTarget(self, action: #selector(recordingButtonTapped), for: .touchUpInside)
+        NSLayoutConstraint.activate([
+            timerLabel.centerXAnchor.constraint(equalTo: borderView.centerXAnchor),
+            timerLabel.topAnchor.constraint(equalTo: borderView.bottomAnchor, constant: 5)
+        ])
+    }
+    
+    private func connectTarget() {
+        recordingButton.addTarget(self, action: #selector(recordingButtonTapped), for: .touchUpInside)
+    }
+    
+    // MARK: - Timer methods
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in
+            guard let self else { return }
+            
+            self.secondsOfTimer += 1
+            self.timerLabel.text = Double(self.secondsOfTimer).format(units: [.minute, .second])
+        }
+    }
+    
+    private func stopTimer() {
+        timer?.invalidate()
+        secondsOfTimer = 0
+        self.timerLabel.text = "00:00"
     }
     
     @objc
     func recordingButtonTapped() {
-        recodingButton.isSelected.toggle()
-        UIView.animate(withDuration: 1, delay: 0, options: [.curveEaseInOut], animations: {
-            if self.recodingButton.isSelected {
+        recordingButton.isSelected.toggle()
+        
+        recordingButton.isSelected ? self.startTimer() : self.stopTimer()
+        
+        UIView.animate(withDuration: 0.4, delay: 0, options: [.curveEaseInOut], animations: {
+            if self.recordingButton.isSelected {
                 self.buttonWidthConstraint.constant = Constant.RecordingButtonWidth
                 self.buttonHeightConstraint.constant = Constant.RecordingButtonWidth
-                self.recodingButton.layer.cornerRadius = Constant.recordingButtonRadius
+                self.recordingButton.layer.cornerRadius = Constant.recordingButtonRadius
             } else {
                 self.buttonWidthConstraint.constant = Constant.ButtonWidth
                 self.buttonHeightConstraint.constant = Constant.ButtonWidth
-                self.recodingButton.layer.cornerRadius = Constant.buttonRadius
+                self.recordingButton.layer.cornerRadius = Constant.buttonRadius
             }
             self.view.layoutIfNeeded()
         }, completion: nil)
     }
 }
+
+fileprivate extension Double {
+    func format(units: NSCalendar.Unit) -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = units
+        formatter.zeroFormattingBehavior = [.pad]
+        
+        return formatter.string(from: self)!
+    }
+}
+
