54068ba742d38e470d6d911da1d793aa40e7389d
seunghyunCheon <csh7215@naver.com>
2023-06-10 13:02:59 +0900

    feat: VideoGenrerator 객체 구현

--- ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/RecordingScene/VideoGenerator.swift
+++ ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/RecordingScene/VideoGenerator.swift
@@ -7,21 +7,28 @@
 import Foundation
 
 struct VideoGenerator {
-    func makeVideo(videoURL: URL, duration: String, imageData: Data) -> VideoEntity? {
+    func makeVideo(videoURL: URL, duration: Int, imageData: Data) -> VideoEntity? {
         guard let uuid = extractUUID(from: videoURL) else { return nil }
-        let tempVideoName = String(uuid.suffix(6))
+        let videoName = String(uuid.uuidString.suffix(6))
+        let videoDuration = Double(duration).format(units: [.minute, .second])
         
-        
-        return nil
+        return VideoEntity(
+            id: uuid,
+            name: videoName,
+            date: Date(),
+            duration: videoDuration ?? "00:00",
+            thumbnail: imageData,
+            videoURL: videoURL
+        )
     }
     
-    private func extractUUID(from filePath: URL) -> String? {
+    private func extractUUID(from filePath: URL) -> UUID? {
         let fileName = filePath.lastPathComponent
         let components = fileName.components(separatedBy: ".")
         
         guard components.count >= 2 else { return nil }
         
-        return components[0]
+        return UUID(uuidString: components[0])
     }
     
     func makeVideoName() {
