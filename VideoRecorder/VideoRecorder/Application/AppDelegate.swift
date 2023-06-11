5bf9986720ce5185413139919ff5f015f82c1750
kkuldak1@naver.com <kkuldak1@naver.com>
2023-06-11 17:21:34 +0900

    feat: 불필요한 출력 코드 삭제

--- VideoRecorder/VideoRecorder/Application/AppDelegate.swift
+++ VideoRecorder/VideoRecorder/Application/AppDelegate.swift
@@ -17,12 +17,12 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        print(UIDevice.current.name)
-        print(UIDevice.current.systemName)
+        
         return true
     }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
